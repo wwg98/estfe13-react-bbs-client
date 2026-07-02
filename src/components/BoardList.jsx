588480028f00a36ec3d7009f8132682ef3954b83
wwg98 <wwwg98@gmail.com>
2026-07-02 15:40:29 +0900

    Refactor BoardList component to fetch and display data from API, removing hardcoded entries

--- src/components/BoardList.jsx
+++ src/components/BoardList.jsx
@@ -2,21 +2,40 @@ import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import axios from "axios";
+import { useEffect, useState } from "react";
+
+function Board({ data }) {
+  return (
+    <tr>
+      <td>
+        <Form.Check />
+      </td>
+      <td>{data.id}</td>
+      <td>{data.title}</td>
+      <td>{data.writer}</td>
+      <td>{data.date}</td>
+    </tr>
+  );
+}
 
 export default function BoardList() {
-  const simpleTest = () => {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
     axios
-      .get("http://localhost:3000/", {})
+      .get("http://localhost:3000/list", {})
       .then(response => {
-        console.log(response);
+        console.log(response.data);
+        setList(response.data);
       })
       .catch(error => {
         console.error(error);
       })
       .finally(() => {
-        console.log("Request completed");
+        console.log("데이터 요청 완료");
       });
-  };
+  }, []);
+
   return (
     <>
       <Table striped bordered hover>
@@ -30,30 +49,13 @@ export default function BoardList() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <Form.Check disabled />
-            </td>
-            <td>1</td>
-            <td>안녕</td>
-            <td>홍길동</td>
-            <td>2026.07.02</td>
-          </tr>
-          <tr>
-            <td>
-              <Form.Check disabled />
-            </td>
-            <td>2</td>
-            <td>안녕하세요</td>
-            <td>김철수</td>
-            <td>2026.07.03</td>
-          </tr>
+          {list.map(item => (
+            <Board key={item.id} data={item} />
+          ))}
         </tbody>
       </Table>
       <div className="d-flex gap-1 justify-content-end">
-        <Button variant="primary" onClick={simpleTest}>
-          입력
-        </Button>
+        <Button variant="primary">입력</Button>
         <Button variant="secondary">수정</Button>
         <Button variant="danger">삭제 </Button>
       </div>
