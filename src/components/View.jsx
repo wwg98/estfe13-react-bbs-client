c806507e3999fb257a9f1dc6575a8cbe236ef72a
wwg98 <wwwg98@gmail.com>
2026-07-08 13:24:02 +0900

    Fix image source path in View and Write components for consistency

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -85,7 +85,7 @@ export default function View({ handleModify }) {
       {content.content}
       {content.image && (
         <div>
-          <img src={`/${content.image}`} alt={content.title} style={{ maxWidth: "80%" }} />
+          <img src={`${content.image}`} alt={content.title} style={{ maxWidth: "80%" }} />
         </div>
       )}
       <hr />
